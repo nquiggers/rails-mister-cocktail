d83af6b9b43a30c73c6b431db459f5c015b01088
Neil Quigley <neil.quigley@me.com>
2020-11-13 15:59:48 +0000

    added env file

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,9 +7,9 @@ class DosesController < ApplicationController
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.cocktail_id = @cocktail.id
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_path(@cocktail.id)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,9 @@ require("channels")
 
 // app/javascript/packs/application.js
 import 'bootstrap';
+import improveDropdown from '../components/select';
+
+improveDropdown();
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +20,4 @@ import 'bootstrap';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
