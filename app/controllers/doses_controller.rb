78d7fcfe8afc7a124a8e7fdc3780fce4a90bcb33
Neil Quigley <neil.quigley@me.com>
2020-11-13 05:58:33 +0000

    pushing back-end set up

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,28 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail_id = @cocktail.id
+    if @dose.save
+      redirect_to cocktail_path(@cocktail.id)
+    else
+      render :new
+    end
+  end
+
+ def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
+  end
+
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
